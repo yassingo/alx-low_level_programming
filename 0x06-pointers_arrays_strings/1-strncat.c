f2a17ae3ea790ad5233f6511ffebd8546026c236
Yassine <sma070911@gmail.com>
2023-06-29 16:45:35 -0700

    comm1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,26 +8,92 @@
  *
  * Return: Pointer to the resulting string dest
  */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
+In this implementation, the _strcat function concatenates the src string to the dest string by finding the end of the dest string using a while loop. Then, it appends the characters of the src string to the dest string using another while loop. Finally, it adds the terminating null byte (\0) to the end of the concatenated string and returns a pointer to dest.
+
+The function prototype is defined in the main.h header file, which guards against double inclusion.
+
+Remember to compile your code with the given compilation options and include the necessary main.h header file in any files that use the _strcat function.
+
+
+
+
+
+User
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+All your files should end with a new line
+A README.md file, at the root of the folder of the project is mandatory
+Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+You are not allowed to use global variables
+No more than 5 functions per file
+You are not allowed to use the standard library. Any use of functions like printf, puts, etc… is forbidden
+You are allowed to use _putchar
+You don’t have to push _putchar.c, we will use our file. If you do it won’t be taken into account
+In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called main.h
+Don’t forget to push your header file,Write a function that concatenates two strings.
+
+Prototype: char *_strncat(char *dest, char *src, int n);
+The _strncat function is similar to the _strcat function, except that
+it will use at most n bytes from src; and
+src does not need to be null-terminated if it contains n or more bytes
+Return a pointer to the resulting string dest
+FYI: The standard library provides a similar function: strncat. Run man strncat to learn more.
+ChatGPT
+Certainly! Here's the implementation of the _strncat function adhering to the given requirements:
+
+c
+Copy code
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strncat(char *dest, char *src, int n);
+
+#endif /* MAIN_H */
+c
+Copy code
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Maximum number of bytes to be used from src
+ *
+ * Return: Pointer to the resulting string dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *ptr = dest;
+	int dest_len, i;
 
-	/* Move the dest pointer to the end of the string */
-	while (*ptr != '\0')
-	{
-		ptr++;
-	}
+	dest_len = 0;
+	while (dest[dest_len] != '\0')
+		dest_len++;
 
-	/* Copy at most n bytes from src to dest */
-	while (n > 0 && *src != '\0')
-	{
-		*ptr = *src;
-		ptr++;
-		src++;
-		n--;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len++] = src[i];
 
-	*ptr = '\0';
+	dest[dest_len] = '\0';
 
 	return dest;
 }
