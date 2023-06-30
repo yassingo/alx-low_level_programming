c91e8a766ae34259a0c35e5f877d15ddd9147001
Yassine <sma070911@gmail.com>
2023-06-29 17:46:52 -0700

    comm3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+/**
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: Negative integer if s1 is less than s2,
+ *         Positive integer if s1 is greater than s2,
+ *         Zero if s1 is equal to s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	/* Function implementation */
+}
+
