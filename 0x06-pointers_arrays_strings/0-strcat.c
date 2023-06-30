ec7ae6317b7f17947ddf1c675d30a962991c3fb3
Yassine <sma070911@gmail.com>
2023-06-29 17:19:57 -0700

    comm0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = 0, src_len = 0, i;
+    int dest_len = 0, src_len = 0, i;
 
-	while (dest[dest_len] != '\0')
-		dest_len++;
+    while (dest[dest_len] != '\0')
+        dest_len++;
 
-	while (src[src_len] != '\0')
-		src_len++;
+    while (src[src_len] != '\0')
+        src_len++;
 
-	for (i = 0; i < src_len; i++)
-		dest[dest_len++] = src[i];
+    for (i = 0; i < src_len; i++)
+        dest[dest_len++] = src[i];
 
-	dest[dest_len] = '\0';
+    dest[dest_len] = '\0';
 
-	return dest;
+    return (dest);
 }
 
