631ab0a61556b02b2dc67316a39c19cc7acce150
Yassine <sma070911@gmail.com>
2023-06-29 19:08:49 -0700

    comm101

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    int n = 98;
+
+    print_number(n);
+
+    return 0;
+}
+
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_number - Prints an integer using only _putchar
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+  /* Your code here */
+}
+
