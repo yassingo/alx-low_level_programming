228fce24cc8d8a526d3d33fdd21ed19a247f536b
Yassine <sma070911@gmail.com>
2023-06-29 17:52:05 -0700

    comm3

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,20 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Entry point
+ * _strcmp - Compare two strings
+ * @s1: First string
+ * @s2: Second string
  *
- * Return: Always 0
+ * Return: Negative value if s1 < s2, 0 if s1 == s2, positive value if s1 > s2
  */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+	int i;
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
 
-	return (0);
+	return (s1[i] - s2[i]);
 }
 
