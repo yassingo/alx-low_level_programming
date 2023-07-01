bbe84111e4d15c98a9b3fd401024daaf8bdb0925
Yassine <sma070911@gmail.com>
2023-06-30 18:09:25 -0700

    comm101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,23 @@
-#include "main.h"
-
-/**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
- */
+#include "holberton.h"
 
 void print_number(int n)
 {
-	unsigned int n1;
+	int divisor = 1;
 
-	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		n = -n;
 	}
-	if (n1 / 10 != 0)
+
+	while (n / divisor >= 10)
+		divisor *= 10;
+
+	while (divisor != 0)
 	{
-		print_number(n1 / 10);
+		_putchar('0' + n / divisor);
+		n %= divisor;
+		divisor /= 10;
 	}
-	_putchar((n1 % 10) + '0');
 }
+
