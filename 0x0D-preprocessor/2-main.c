c187c6c16ef3ba2d8e3e066edc51056149bfb9ca
Yassine <sma070911@gmail.com>
2023-07-17 15:24:37 -0700

    comm0

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    /* Suppress unused parameter warning */
-    (void)argc;
-
-    printf("%s\n", argv[0]);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
