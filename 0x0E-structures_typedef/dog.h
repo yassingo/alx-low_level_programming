4afca9bb0d0814260521b78ef58704e3fafa3c7e
Yassine <sma070911@gmail.com>
2023-07-17 15:55:05 -0700

    comm0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stddef.h>
-
 struct dog
 {
     char *name;
@@ -10,7 +8,7 @@ struct dog
     char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
 
