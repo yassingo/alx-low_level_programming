186b42b8b048cd52061c32503b3f0a4b752a7bc3
yassingoo <sma070911@gmail.com>
2023-07-28 02:03:27 +0100

    comm0

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,13 +7,3 @@ main:
 	  mov   eax, 0
 	  ret
 format: db `Hello, Holberton\n`,0
-global   main
-	  extern    printf
-main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
-
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -16,22 +16,4 @@ void free_list(list_t *head)
 												free(head);
 														head = temp;
 															}
-}#include <stdlib.h>
-#include "lists.h"
-
-/**
- *  * free_list - frees a linked list
- *   * @head: list_t list to be freed
- *    */
-void free_list(list_t *head)
-{
-		list_t *temp;
-
-			while (head)
-					{
-								temp = head->next;
-										free(head->str);
-												free(head);
-														head = temp;
-															}
 }
