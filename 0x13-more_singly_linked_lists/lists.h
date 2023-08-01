8511b6044c7b19cb6fcc66154b47b4d5471b2206
yassingoo <sma070911@gmail.com>
2023-08-01 01:53:13 +0100

    comm0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,16 +2,18 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
 
-/* Structure for a singly linked list node */
+/* Structure for linked list */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-/* Function prototype to print all elements of a linked list */
+/* Function prototypes */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
 
