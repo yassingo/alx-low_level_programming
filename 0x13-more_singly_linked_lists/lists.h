ce26fc2b9ccac2869fc9c4a3c2051b5193c8079d
yassingoo <sma070911@gmail.com>
2023-08-01 02:24:08 +0100

    comm0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,18 +2,23 @@
 #define LISTS_H
 
 #include <stddef.h>
-#include <stdio.h>
 
-/* Structure for linked list */
+/* Structure for the linked list */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int pop_listint(listint_t **head);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LISTS_H */
 
