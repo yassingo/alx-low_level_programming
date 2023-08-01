1e23460434769fca4aabef3c5a5cebd9239436d3
yassingoo <sma070911@gmail.com>
2023-08-02 00:27:08 +0100

    comm0

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,30 +7,30 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tortoise = head, *hare = head;
+    listint_t *tortoise = head, *hare = head;
 
-	if (head == NULL || head->next == NULL)
-		return (NULL);
+    if (head == NULL || head->next == NULL)
+        return (NULL);
 
-	while (hare != NULL && hare->next != NULL)
-	{
-		hare = hare->next->next;
-		tortoise = tortoise->next;
+    while (hare != NULL && hare->next != NULL)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
 
-		if (hare == tortoise)
-		{
-			/* Loop found, reset one of the pointers and start again */
-			hare = head;
-			while (hare != tortoise)
-			{
-				hare = hare->next;
-				tortoise = tortoise->next;
-			}
-			return (tortoise);
-		}
-	}
+        if (hare == tortoise)
+        {
+            /* Loop found, reset one of the pointers and start again */
+            hare = head;
+            while (hare != tortoise)
+            {
+                hare = hare->next;
+                tortoise = tortoise->next;
+            }
+            return (tortoise);
+        }
+    }
 
-	/* If no loop is found, return NULL */
-	return (NULL);
+    /* If no loop is found, return NULL */
+    return (NULL);
 }
 
