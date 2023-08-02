386d679d5d2fdca9a31e1fab73e5a4bb277731fd
yassingoo <sma070911@gmail.com>
2023-08-02 01:25:11 +0100

    comm0

--- 0x13-more_singly_linked_lists/101-main.c
+++ 0x13-more_singly_linked_lists/101-main.c
@@ -4,44 +4,44 @@
 
 int main(void)
 {
-    listint_t *head;
-    listint_t *head2;
-    listint_t *node;
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
 
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
 
-    node = find_listint_loop(head2);
-    if (node != NULL)
-        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-    else
-        printf("No loop\n");
+	node = find_listint_loop(head2);
+	if (node != NULL)
+		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+	else
+		printf("No loop\n");
 
-    head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 5);
-    add_nodeint(&head, 6);
-    add_nodeint(&head, 7);
-    add_nodeint(&head, 8);
-    add_nodeint(&head, 9);
-    print_listint_safe(head);
-    free_listint_safe(&head);
-    printf("-> %p\n", (void *)head);
+	head = NULL;
+	add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	add_nodeint(&head, 5);
+	add_nodeint(&head, 6);
+	add_nodeint(&head, 7);
+	add_nodeint(&head, 8);
+	add_nodeint(&head, 9);
+	print_listint_safe(head);
+	free_listint_safe(&head);
+	printf("-> %p\n", (void *)head);
 
-    node = find_listint_loop(head);
-    if (node != NULL)
-        printf("Loop starts at [%p] %d\n", (void *)node, node->n);
-    else
-        printf("No loop\n");
+	node = find_listint_loop(head);
+	if (node != NULL)
+		printf("Loop starts at [%p] %d\n", (void *)node, node->n);
+	else
+		printf("No loop\n");
 
-    return (0);
+	return (0);
 }
 
